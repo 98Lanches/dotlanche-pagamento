9b35d1e0aae54f5e7c221e5daea4a36ad83024b9
Andre Torres <atcorrea17@gmail.com>
2024-10-10 14:59:38 -0300

    feat: Add initial project structure

---
+++ test/UnitTests/Dotlanche.Pagamento.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Dotlanche.Pagamento.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
