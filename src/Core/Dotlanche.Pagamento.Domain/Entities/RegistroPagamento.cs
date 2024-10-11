e142ac71464d81ac7924949807536a10af096db6
Andre Torres <atcorrea17@gmail.com>
2024-10-11 11:27:47 -0300

    chore: Add Pagamento Entity

---
+++ src/Core/Dotlanche.Pagamento.Domain/Entities/RegistroPagamento.cs
@@ -0,0 +1,38 @@
+﻿using Dotlanche.Pagamento.Domain.Exceptions;
+
+namespace Dotlanche.Pagamento.Domain.Entities
+{
+    public class RegistroPagamento
+    {
+        public Guid Id { get; private set; }
+
+        public decimal Amount { get; }
+
+        public bool IsAccepted { get; private set; }
+
+        public DateTime RegisteredAt { get; private set; }
+
+        public DateTime? AcceptedAt { get; private set; }
+
+        public RegistroPagamento(decimal amount)
+        {
+            Id = Guid.NewGuid();
+            Amount = amount;
+            RegisteredAt = DateTime.Now;
+
+            ValidateEntity();
+        }
+
+        public void ConfirmPayment()
+        {
+            IsAccepted = true;
+            AcceptedAt = DateTime.Now;
+        }
+
+        private void ValidateEntity()
+        {
+            if (Amount < 0)
+                throw new DomainValidationException(nameof(Amount));
+        }
+    }
+}
---
+++ test/UnitTests/Dotlanche.Pagamento.UnitTests/Core/Domain/Entities/RegistroPagamentoTests.cs
@@ -0,0 +1,56 @@
+﻿using Dotlanche.Pagamento.Domain.Entities;
+using Dotlanche.Pagamento.Domain.Exceptions;
+using FluentAssertions;
+
+namespace Dotlanche.Pagamento.UnitTests.Core.Domain.Entities
+{
+    public class RegistroPagamentoTests
+    {
+        [Test]
+        public void New_WhenAmountIsLowerThanZero_ShouldThrowDomainValidationException()
+        {
+            // Arrange
+            const decimal amount = -5;
+
+            // Act
+            var newPagamentoCall = () => new RegistroPagamento(amount);
+
+            // Assert
+            newPagamentoCall
+                .Should()
+                .Throw<DomainValidationException>()
+                .WithMessage("invalid value for Amount!");
+        }
+
+        [Test]
+        public void New_WhenNoValidationErrorOccurs_ShouldCreateObjectWithCorrectValues()
+        {
+            // Arrange
+            const decimal amount = 10;
+
+            // Act
+            var pagamento = new RegistroPagamento(amount);
+
+            // Assert
+            pagamento.Id.Should().NotBeEmpty();
+            pagamento.Amount.Should().Be(amount);
+            pagamento.IsAccepted.Should().BeFalse();
+            pagamento.RegisteredAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+            pagamento.AcceptedAt.Should().BeNull();
+        }
+
+        [Test]
+        public void ConfirmPayment_WhenCalled_ShouldSetIsAcceptedToTrueAndAssignAcceptedAtValue()
+        {
+            // Arrange
+            var pagamento = new RegistroPagamento(amount: 50);
+
+            // Act
+            pagamento.ConfirmPayment();
+
+            // Assert
+            pagamento.IsAccepted.Should().BeTrue();
+            pagamento.AcceptedAt.Should().BeCloseTo(DateTime.Now, TimeSpan.FromSeconds(1));
+        }
+    }
+}
