26b2e80db3e5227d22740856ed85af8dbc5d43a8
Andre Torres <atcorrea17@gmail.com>
2024-10-22 19:54:06 -0300

    feat: Add postgresql healthcheck

--- src/Drivers/Dotlanche.Pagamento.WebApi/Program.cs
+++ src/Drivers/Dotlanche.Pagamento.WebApi/Program.cs
@@ -1,6 +1,7 @@
 using Dotlanche.Pagamento.Application.DependencyInjection;
 using Dotlanche.Pagamento.Checkout.DependencyInjection;
 using Dotlanche.Pagamento.Data.DependencyInjection;
+using Dotlanche.Pagamento.WebApi.Exceptions;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,11 +15,18 @@ builder.Services.Configure<Microsoft.AspNetCore.Mvc.JsonOptions>(options =>
     options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
 });
 
+builder.Services
+    .AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection") ??
+        throw new MisconfigurationException("ConnectionStrings.DefaultConnection"));
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.MapHealthChecks("/health");
 
 app.UseSwagger();
 app.UseSwaggerUI();
